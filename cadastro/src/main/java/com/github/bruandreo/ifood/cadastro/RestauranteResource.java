1f1961d54510665855102a5e7a9e957db79e656a
Bruno Andreo <brunolino2026@gmail.com>
2022-12-06 22:46:09 -0300

    feat: added authentication in Restaurante Resource

--- cadastro/src/main/java/com/github/bruandreo/ifood/cadastro/RestauranteResource.java
+++ cadastro/src/main/java/com/github/bruandreo/ifood/cadastro/RestauranteResource.java
@@ -4,10 +4,16 @@ import com.github.bruandreo.ifood.cadastro.dto.RestauranteAddDTO;
 import com.github.bruandreo.ifood.cadastro.dto.RestauranteEditDTO;
 import com.github.bruandreo.ifood.cadastro.dto.RestauranteMapper;
 import com.github.bruandreo.ifood.cadastro.infra.ConstraintViolationResponse;
+import org.eclipse.microprofile.openapi.annotations.enums.SecuritySchemeType;
 import org.eclipse.microprofile.openapi.annotations.media.Content;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.security.OAuthFlow;
+import org.eclipse.microprofile.openapi.annotations.security.OAuthFlows;
+import org.eclipse.microprofile.openapi.annotations.security.SecurityRequirement;
+import org.eclipse.microprofile.openapi.annotations.security.SecurityScheme;
 
+import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
@@ -20,6 +26,13 @@ import java.util.Optional;
 @Path("/restaurantes")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
+@RolesAllowed("proprietario")
+@SecurityScheme(
+    securitySchemeName = "ifood-oauth",
+    type = SecuritySchemeType.OAUTH2,
+    flows = @OAuthFlows(password = @OAuthFlow(tokenUrl = "http://localhost:8180/auth/realms/ifood/protocol/openid-connect/token"))
+)
+@SecurityRequirement(name = "ifood-oauth")
 public class RestauranteResource {
 
     @Inject
